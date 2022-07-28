1448f897c7cf348b25bc91201d7df3243b585093
salehmu <ghd@keemail.me>
2022-07-28 14:56:04 +0200

    intial

---
+++ go.mod
@@ -0,0 +1,13 @@
+module tesseract-to-xclip
+
+go 1.18
+
+require (
+	github.com/otiai10/gosseract/v2 v2.4.0
+	golang.design/x/clipboard v0.6.2 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/exp/shiny v0.0.0-20220722155223-a9213eeb770e // indirect
+	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539 // indirect
+	golang.org/x/mobile v0.0.0-20220722155234-aaac322e2105 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"github.com/otiai10/gosseract/v2"
+	"golang.design/x/clipboard"
+	_ "image/png"
+	"io/ioutil"
+	"os"
+)
+
+var filename string = "/tmp/tesseract.png"
+
+func main() {
+
+	lng := "ara"
+	if len(os.Args) > 0 {
+		lng = "eng"
+
+	}
+	var client *gosseract.Client = gosseract.NewClient()
+	defer func(client *gosseract.Client) {
+		err := client.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(client)
+
+	err := client.SetLanguage(lng)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		changed := clipboard.Watch(context.Background(), clipboard.FmtImage)
+		select {
+		case <-changed:
+
+			d := clipboard.Read(clipboard.FmtImage)
+			_ = ioutil.WriteFile(filename, d, 0644)
+
+			if err != nil {
+				println("fcu")
+				panic(err)
+			} else {
+				readimage(client)
+				text, _ := client.Text()
+				clipboard.Write(clipboard.FmtText, []byte(text))
+			}
+		}
+	}
+
+}
+
+func readimage(client *gosseract.Client) {
+
+	err := client.SetImage(filename)
+	if err != nil {
+		panic(err)
+	}
+}
